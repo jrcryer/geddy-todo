320b6ad606560425b2e68c447a975e7993ddf082
James Cryer <chat@jamescryer.com>
2012-04-09 10:40:01 +0100

    Init commit

---
+++ app/models/todo.js
@@ -0,0 +1,15 @@
+
+var Todo = function () {
+
+  this.property('id', 'string', {required: true});
+  this.property('title', 'string', {required: true});
+  this.property('status', 'string', {required: true});
+
+  this.validatesPresent('title');
+  this.validatesLength('title', {min: 5});
+  this.validatesWithFunction('status', function(status) {
+      return status == 'open' || status == 'done';
+  });
+};
+Todo = geddy.model.register('Todo', Todo);
+
